aa0d5a9f8de13fe2f0261c56abe98b6c6dca667e
NashLegend <panzhihuipzh@gmail.com>
2014-03-13 20:06:28 +0800

    Init

---
+++ src/com/example/legendutils/DisplayUtils.java
@@ -0,0 +1,9 @@
+package com.example.legendutils;
+
+public class DisplayUtils {
+
+	public DisplayUtils() {
+		// TODO 自动生成的构造函数存根
+	}
+
+}
---
+++ src/com/example/legendutils/MathUtils.java
@@ -0,0 +1,9 @@
+package com.example.legendutils;
+
+public class MathUtils {
+
+	public MathUtils() {
+		// TODO 自动生成的构造函数存根
+	}
+
+}
