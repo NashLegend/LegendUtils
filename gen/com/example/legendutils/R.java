000b449703f1674787b45c488acbec16821c64ce
NashLegend <panzhihuipzh@gmail.com>
2014-03-21 16:26:50 +0800

    EmptyFileDialog

--- gen/com/example/legendutils/R.java
+++ gen/com/example/legendutils/R.java
@@ -13,18 +13,30 @@ public final class R {
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
+    public static final class id {
+        public static final int button_OK=0x7f060001;
+        public static final int picker=0x7f060000;
+    }
+    public static final class layout {
+        public static final int dialog_number_picker=0x7f030000;
+        public static final int dialog_open_file=0x7f030001;
+    }
     public static final class string {
-        public static final int app_name=0x7f030000;
+        public static final int app_name=0x7f040000;
+        public static final int file_opener=0x7f040002;
+        public static final int number_picker=0x7f040001;
     }
     public static final class style {
         /** 
         Base application theme, dependent on API level. This theme is replaced
         by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
+
     
 
             Theme customizations available in newer API levels can go in
             res/values-vXX/styles.xml, while customizations related to
             backward-compatibility can go here.
+
         
 
         Base application theme for API 11+. This theme completely replaces
@@ -38,10 +50,11 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f040000;
+        public static final int AppBaseTheme=0x7f050000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f040001;
+        public static final int AppTheme=0x7f050001;
+        public static final int Dialog=0x7f050002;
     }
 }
--- src/com/example/legendutils/Views/NumberPickerDialog.java
+++ src/com/example/legendutils/Views/NumberPickerDialog.java
@@ -1,8 +1,22 @@
+
 package com.example.legendutils.Views;
 
-import android.app.Dialog;
-import android.content.Context;
+import com.example.legendutils.R;
 
+import android.annotation.TargetApi;
+import android.app.Dialog;
+import android.app.Service;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Build;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.NumberPicker;
+
+@TargetApi(Build.VERSION_CODES.HONEYCOMB)
 public class NumberPickerDialog extends Dialog {
 
     public NumberPickerDialog(Context context) {
@@ -18,6 +32,66 @@ public class NumberPickerDialog extends Dialog {
     public NumberPickerDialog(Context context, boolean cancelable, OnCancelListener cancelListener) {
         super(context, cancelable, cancelListener);
         // TODO 自动生成的构造函数存根
+    }
+
+    public interface onNumberSetListener {
+        public void onNumberSet(DialogInterface diaalog, int num);
+    }
+
+    public static class Builder {
+        private Context context;
+        private NumberPicker picker;
+        private Button button;
+        private int max = 0;
+        private int min = 0;
+        private onNumberSetListener numberSetListener;
+
+        public Builder(Context context) {
+            this.context = context;
+        }
+
+        public Builder setNumberSetListener(onNumberSetListener listener) {
+            numberSetListener = listener;
+            return this;
+        }
+
+        public Builder setMaxValue(int max) {
+            this.max = max;
+            return this;
+        }
+
+        public Builder setMinValue(int min) {
+            this.min = min;
+            return this;
+        }
+
+        public NumberPickerDialog create() {
+            final NumberPickerDialog dialog = new NumberPickerDialog(context, R.style.Dialog);
+            LayoutInflater inflater = (LayoutInflater) context
+                    .getSystemService(Service.LAYOUT_INFLATER_SERVICE);
+            View layout = inflater.inflate(R.layout.dialog_number_picker, null);
+            dialog.addContentView(layout, new LayoutParams(LayoutParams.MATCH_PARENT,
+                    LayoutParams.WRAP_CONTENT));
+            picker = (NumberPicker) layout.findViewById(R.id.picker);
+            picker.setMaxValue(max);
+            picker.setMinValue(min);
+            button = (Button) layout.findViewById(R.id.button_OK);
+            button.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+                    // TODO 自动生成的方法存根
+                    if (numberSetListener != null) {
+                        picker.clearFocus();// if not clearFocus, u can't get
+                                            // the value by softKeyBoard from
+                                            // the EditText
+                        numberSetListener.onNumberSet(dialog, picker.getValue());
+                    }
+                }
+            });
+            dialog.setContentView(layout);
+            return dialog;
+        }
     }
 
 }
