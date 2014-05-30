914470fd7557e2d14442176f0d0e946854f9ad9b
NashLegend <panzhihuipzh@gmail.com>
2014-05-30 11:34:12 +0800

    timerutil

--- src/com/example/legendutils/Tools/TimerUtil.java
+++ src/com/example/legendutils/Tools/TimerUtil.java
@@ -3,41 +3,23 @@ package com.example.legendutils.Tools;
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class ThreadUtil {
+public class TimerUtil {
 
-	public ThreadUtil() {
+	public TimerUtil() {
 
-	}
-
-	public static Timer setTimeOut(TimerTask runnable, int delayMillis) {
-		Timer timer = new Timer();
-		timer.schedule(runnable, delayMillis);
-		return timer;
 	}
 
 	public static Timer setTimeOut(final Runnable runnable, int delayMillis) {
 
-		Timer timer = new Timer();
+		final Timer timer = new Timer();
 		timer.schedule(new TimerTask() {
 
 			@Override
 			public void run() {
 				runnable.run();
+				timer.cancel();
 			}
 		}, delayMillis);
-		return timer;
-	}
-
-	public static void clearTimeOut(Timer timer) {
-		if (timer != null) {
-			timer.cancel();
-		}
-	}
-
-	public static Timer setInterval(TimerTask runnable, int delayMillis,
-			int period) {
-		Timer timer = new Timer();
-		timer.schedule(runnable, delayMillis, period);
 		return timer;
 	}
 
@@ -54,6 +36,12 @@ public class ThreadUtil {
 	}
 
 	public static void clearInterval(Timer timer) {
+		if (timer != null) {
+			timer.cancel();
+		}
+	}
+
+	public static void clearTimeOut(Timer timer) {
 		if (timer != null) {
 			timer.cancel();
 		}
