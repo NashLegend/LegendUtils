824b68136596520ae0b7f3e3fc66c786d0aa99e9
NashLegend <panzhihuipzh@gmail.com>
2014-06-09 19:26:59 +0800

    WPD

---
+++ src/com/example/legendutils/Dialogs/Win8ProgressDialog.java
@@ -0,0 +1,30 @@
+package com.example.legendutils.Dialogs;
+
+import android.app.Dialog;
+import android.content.Context;
+
+/**
+ * Win8样式的loading
+ * 
+ * @author NashLegend
+ * 
+ */
+public class Win8ProgressDialog extends Dialog {
+
+	public Win8ProgressDialog(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+
+	public Win8ProgressDialog(Context context, int theme) {
+		super(context, theme);
+		// TODO Auto-generated constructor stub
+	}
+
+	public Win8ProgressDialog(Context context, boolean cancelable,
+			OnCancelListener cancelListener) {
+		super(context, cancelable, cancelListener);
+		// TODO Auto-generated constructor stub
+	}
+
+}
