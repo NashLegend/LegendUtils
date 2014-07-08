84876c39b63e2e6a139357509a342a8978af7c99
NashLegend <panzhihuipzh@gmail.com>
2014-07-08 10:15:33 +0800

    too much

--- src/com/example/legendutils/Tools/SystemUtil.java
+++ src/com/example/legendutils/Tools/SystemUtil.java
@@ -16,15 +16,13 @@ public class SystemUtil {
 
 	public static boolean findBinary(String binaryName) {
 		boolean found = false;
-		if (!found) {
-			String[] places = { "/sbin/", "/system/bin/", "/system/xbin/",
-					"/data/local/xbin/", "/data/local/bin/",
-					"/system/sd/xbin/", "/system/bin/failsafe/", "/data/local/" };
-			for (String where : places) {
-				if (new File(where + binaryName).exists()) {
-					found = true;
-					break;
-				}
+		String[] places = { "/sbin/", "/system/bin/", "/system/xbin/",
+				"/data/local/xbin/", "/data/local/bin/", "/system/sd/xbin/",
+				"/system/bin/failsafe/", "/data/local/" };
+		for (String where : places) {
+			if (new File(where + binaryName).exists()) {
+				found = true;
+				break;
 			}
 		}
 		return found;
@@ -61,7 +59,7 @@ public class SystemUtil {
 					out.close();
 				}
 			} catch (Exception e) {
-				
+
 			}
 		}
 		return reader;
